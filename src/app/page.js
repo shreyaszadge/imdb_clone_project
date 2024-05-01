940b6e1468b100cc39f985ef21e53e883d32c2c3
shreyaszadge <zadgeshreyas@gmail.com>
2024-05-01 12:28:22 +0530

    po

--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,29 @@
-'use client'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Results from '@/components/Results';
+import  Results from "@/components/Results";
 
-const API_KEY = process.env.API_KEY;
+// import React from 'react'
+const API_KEY=process.env.API_KEY;
 
-export async function getServerSideProps({ query }) {
-  const { genre } = query;
-  const selectedGenre = genre || 'fetchTrending';
+const page = async ({searchParams}) => {
+  const genre =searchParams.genre || 'fetchTrending'
+  const res =await fetch(`https://api.themoviedb.org/3${
+    genre === `fetchTrending` ? `/movie/top_rated` : `/trending/all/week`
+  }?api_key=${API_KEY}&languageen-US&page=1`,{next:{revalidate:10000}})
 
-  try {
-    const res = await fetch(`https://api.themoviedb.org/3${
-      selectedGenre === 'fetchTrending' ? '/movie/top_rated' : '/trending/all/week'
-    }?api_key=${API_KEY}&language=en-US&page=1`);
-    
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    
-    const data = await res.json();
-    const results = data.results;
 
-    return {
-      props: {
-        results,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      props: {
-        results: [],
-        error: 'Failed to fetch data',
-      },
-    };
+  const data = await res.json()
+  if(!res.ok){
+    throw new Error('failed to fetch')
   }
+  const result1 = data.results;
+  // console.log(result)
+  return (
+    <div>Home 
+
+
+      <Results results={result1}/>
+    </div>
+  )
 }
 
-const Page = ({ results, error }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // You can use router.query.genre here if you want to access the genre from the query params
-  }, [router.query]);
-
-  return (
-    <div>
-      {error ? (
-        <div>Error: {error}</div>
-      ) : (
-        <div>
-          <h1>Home</h1>
-          <Results results={results} />
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Page;
+export default page
 
