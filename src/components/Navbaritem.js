2a021d885a8963ca87f5cf5b14f23d008732aff1
shreyaszadge <zadgeshreyas@gmail.com>
2024-05-01 12:42:30 +0530

    dd

--- src/components/Navbaritem.js
+++ src/components/Navbaritem.js
@@ -1,21 +1,32 @@
 'use client'
 
 import Link from "next/link"
-import { useSearchParams } from "next/navigation"
-const Navbaritem = ({title,param}) => {
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
-    const search =useSearchParams();
-    const genre =search.get('genre')
+const Navbaritem = ({ title, param }) => {
 
-  return (
-    <div>
-
-        <Link 
-        className={`hover:text-amber-600 font-semibold ${genre === param ?'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg ':''} `}
-  
-        href ={`/?genre=${param}`} >{title}</Link>
-    </div>
-  )
+    return (
+        <div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <NavbarLink title={title} param={param} />
+            </Suspense>
+        </div>
+    );
 }
 
-export default Navbaritem
+const NavbarLink = ({ title, param }) => {
+    const search = useSearchParams();
+    const genre = search.get('genre');
+
+    return (
+        <Link
+            className={`hover:text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg ' : ''}`}
+            href={`/?genre=${param}`}
+        >
+            {title}
+        </Link>
+    );
+}
+
+export default Navbaritem;
